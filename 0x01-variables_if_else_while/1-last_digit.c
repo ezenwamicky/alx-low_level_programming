9dbec4519eb84c15d5fb9685c80064730496ebe3
ezenwamicky <ezenwamagnanimous419@gmail.com>
2023-10-07 04:01:08 -0700

    my first private no cheat code

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return : return 0 if successful followed by a new line
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if ((n % 10) > 5)
+	{
+		printf("%d is %d and is greater than 5\n", n, n % 10);
+	}
+	else if ((n % 10) == 0)
+	{
+		printf("%d is %d and is 0\n", n, n % 10);
+	}
+	else
+	{
+		printf("%d is %d and is less than 6 and not 0\n", n, n % 10);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+This is a my project file on 
+#C-Variable,if,else,while
