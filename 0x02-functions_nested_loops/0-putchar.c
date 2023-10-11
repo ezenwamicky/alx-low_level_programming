5823983df8107b1ee03718e3ff22a5f9257789c3
ezenwamicky <ezenwamagnanimous419@gmail.com>
2023-10-11 03:17:00 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - this prints '_putchar' using function
+ *_putchar which has been included in the prototype
+ *
+ *Return: always(0)success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+0x02.c - Functions, nested loops
+readme project file
+
