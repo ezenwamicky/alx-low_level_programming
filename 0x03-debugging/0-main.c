e08b7a562546a713810290fcecc209c99ff8af84
ezenwamicky <ezenwamagnanimous419@gmail.com>
2023-10-12 14:02:57 -0700

    wow code

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *main - this is used to testand print the negative or positive
+ *Return: always 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Debugging project on C
